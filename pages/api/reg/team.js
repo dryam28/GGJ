b55a4bb462804ae47a1f8b8d21d0714a40154c20
Daniel Chaviano Perez <chavianodaniel99@gmail.com>
2022-11-08 09:34:03 -0500

    almost finished team

--- pages/api/reg/team.js
+++ pages/api/reg/team.js
@@ -4,39 +4,42 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    //Ask for team Team
+    // Check Team Name
     const team_check = await prisma.team.findFirst({
       where: { name: req.body.team.name }
     });
     if (team_check != null) {
-      res.status(409)
-      res.send("Error team already found!");
+      res.status(409);
+      res.send('Error team already Taken!');
       return;
     }
+    //Check Participants Data
+    var user_found = req.body.participants.every(async (item) => {
+      
+      const id_check = await prisma.participant.findFirst({
+        where: { id: item.id }
+      });
+      const email_check = await prisma.participant.findFirst({
+        where: { email: item.email }
+      });
 
-    req.body.participants.map(async (item) => {
-      const id_check = await prisma.team.findFirst({
-        where: { name: req.body.participant.id }
-      });
-      const email_check = await prisma.team.findFirst({
-        where: { name: req.body.participant.email }
-      });
-      const phone_check = await prisma.team.findFirst({
-        where: { name: req.body.participant.email }
-      });
-      if (id_check != null||email_check != null||phone_check != null) {
-        res.status(409)
-        res.send("Error team already found!");
-        return;
+      if (id_check !== null || email_check !== null) {
+        return true;
       }
 
-      console.log(team);
+      return false;
     });
-
+    console.log(user_found);
+    if(user_found===true){
+      res.status(409);
+      res.send('Error participant already found!');
+      return;
+    }
     // id_team Int
     // name String
     // pax Int (no necesario)
     //Save team
+    const team = await prisma.team.create({ data: req.body.team });
 
     //Ask for Participants 4
     // id String
@@ -49,13 +52,11 @@ export default async function handler(req, res) {
     // area String
     // ext Boolean
     //Save Participants
-    const team = await prisma.team.create({ data: req.body.team });
     req.body.participants.map(async (item) => {
       item['team_id'] = team.id_team;
-      item['ext'] = false;
       item['phone'] = String(item['phone']);
       const user = await prisma.participant.create({ data: item });
-      console.log(user);
+      // console.log(user);
     });
 
     res.send(200);
