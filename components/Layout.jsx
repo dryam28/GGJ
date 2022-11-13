ea06fd29c9cc3b10a49f4a695873d760e592c5fd
Adrian <aracosta2810@gmail.com>
2022-11-13 09:57:50 -0500

    New Funtionalities: - Some design bug fixed -Global state with easy-peasey added -Send data input -Others

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ const Layout = ({ children }) => {
 		<div className="container-fluid p-0">
 			<div className="row p-0 m-0" style={{ height: "100vh" }}>
 				<div className="d-none d-md-block col-md-5 logoPic"></div>
-				<div className="col-12 col-md-7 h-100 p-0">
+				<div className="col-12 col-lg-7 h-100 p-0">
 					<div>{children}</div>
 				</div>
 			</div>
--- components/RegisterForms/MembersData.jsx
+++ components/RegisterForms/MembersData.jsx
@@ -1,55 +1,142 @@
+import { useStoreActions, useStoreState } from "easy-peasy";
+import { useState } from "react";
+
 const MembersData = ({ handleNext }) => {
+	const participants = useStoreState((state) => state.participants);
+	const setParticipant = useStoreActions((actions) => actions.setParticipant);
+	const [cont, setCont] = useState(0);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (cont === 3) {
+			handleNext(2);
+			return;
+		}
+		setCont(cont + 1);
+	};
+
 	return (
-		<>
+		<form onSubmit={handleSubmit}>
 			<div className="mb-4">
 				<label htmlFor="exampleFormControlInput1" className="form-label">
 					Nombre
 				</label>
-				<input type="text" className="form-control" />
+				<input
+					autoFocus
+					type="text"
+					name="name"
+					className="form-control"
+					value={participants[cont].name}
+					onChange={(e) =>
+						setParticipant({ index: cont, data: e.target.value, key: "name" })
+					}
+				/>
 			</div>
 
 			<div className="row mb-4">
 				<div className="col-6 mb-4">
 					<label className="form-label">Carne de Identidad</label>
-					<input type="number" className="form-control" />
+					<input
+						type="text"
+						name="CI"
+						className="form-control"
+						value={participants[cont].id}
+						onChange={(e) =>
+							setParticipant({ index: cont, data: e.target.value, key: "id" })
+						}
+					/>
 				</div>
 				<div className="col-6 mb-4">
 					<label className="form-label">Género</label>
-					<select class="form-select" aria-label="Default select example">
+					<select className="form-select" aria-label="Default select example">
 						<option value="1">Masculino</option>
 						<option value="2">Femenino</option>
 						<option value="3">Otro</option>
 					</select>
 				</div>
-				<div className="col-6">
+				<div className="col-12 col-sm-6">
 					<label className="form-label">Número de teléfono</label>
-					<input type="number" className="form-control" />
+					<input
+						type="number"
+						className="form-control"
+						value={participants[cont].phone}
+						onChange={(e) =>
+							setParticipant({
+								index: cont,
+								data: e.target.value,
+								key: "phone",
+							})
+						}
+					/>
 				</div>
-				<div className="col-6">
+				<div className="col-12 col-sm-6 mt-4 mt-sm-0">
 					<label className="form-label">Correo</label>
-					<input type="email" className="form-control" />
+					<input
+						type="email"
+						className="form-control"
+						value={participants[cont].email}
+						onChange={(e) =>
+							setParticipant({
+								index: cont,
+								data: e.target.value,
+								key: "email",
+							})
+						}
+					/>
 				</div>
 			</div>
 			<div className="mb-4">
 				<label className="form-label">Institución</label>
-				<select class="form-select" aria-label="Default select example">
+				<select className="form-select" aria-label="Default select example">
 					<option value="1">1</option>
 					<option value="2">2</option>
 					<option value="3">3</option>
 				</select>
 			</div>
-			<div className="mb-4">
-				<label htmlFor="exampleFormControlInput1" className="form-label">
-					área
-				</label>
-				<input type="text" className="form-control" />
+			<div className="mb-4 row">
+				<div className="col-8">
+					<label htmlFor="exampleFormControlInput1" className="form-label">
+						área
+					</label>
+					<input
+						type="text"
+						className="form-control"
+						value={participants[cont].area}
+						onChange={(e) =>
+							setParticipant({
+								index: cont,
+								data: e.target.value,
+								key: "area",
+							})
+						}
+					/>
+				</div>
+				<div className="col-4 mt-auto">
+					<input
+						className="form-check-input"
+						type="checkbox"
+						value=""
+						id="flexCheckDefault"
+						checked={participants[cont].ext}
+						onChange={(e) =>
+							setParticipant({
+								index: cont,
+								data: !participants[cont].ext,
+								key: "ext",
+							})
+						}
+					/>
+					<label className="form-check-label ms-2" htmlFor="flexCheckDefault">
+						Externo
+					</label>
+				</div>
 			</div>
 
 			<div className="progress-bar mt-5">
 				<div className="progress" style={{ width: "60%" }}></div>
 			</div>
 			<div className="text-center d-lg-none mt-3">
-				datos del primer integrante
+				datos del {cont + 1} integrante
 			</div>
 			<div
 				style={{
@@ -66,16 +153,13 @@ const MembersData = ({ handleNext }) => {
 				</button>
 
 				<div className="text-center d-none d-lg-block">
-					datos del primer integrante
+					datos del {cont + 1} integrante
 				</div>
-				<button
-					className="btn  btn-primary GG_button px-5"
-					onClick={() => handleNext(2)}
-				>
+				<button type="submit" className="btn  btn-primary GG_button px-5">
 					Siguiente
 				</button>
 			</div>
-		</>
+		</form>
 	);
 };
 
