4790b14027585ca2fd5ee8acc71b873b9675ff31
Daniel Chaviano Perez <chavianodaniel99@gmail.com>
2022-11-26 19:19:26 -0500

    nexter finished

--- pages/api/reg/nexter.js
+++ pages/api/reg/nexter.js
@@ -4,14 +4,16 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const id_check = await prisma.participant.findFirst({
+    const id_check = await prisma.ParticipantNext.findFirst({
       where: { id: req.body.id }
     });
 
-    if (id_check !== null || email_check !== null) {
-      return true;
+    if (id_check !== null) {
+      res.send('Error kid already registered!');
+      return;
     }
-
+    const nexter = await prisma.ParticipantNext.create({ data: req.body });
+    
     res.send(200);
   } else {
     console.log();
